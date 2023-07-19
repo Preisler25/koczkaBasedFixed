c54841708b0931f8689e9b5f959b2dca783d016e
Preisler <preisler.andras@gmail.com>
2023-07-19 10:40:18 +0200

    inti reapo

---
+++ src/main/java/petrik/dev/koczkaBased/KoczkaBasedApplication.java
@@ -0,0 +1,13 @@
+package petrik.dev.koczkaBased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoczkaBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoczkaBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/petrik/dev/koczkaBased/KoczkaBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package petrik.dev.koczkaBased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoczkaBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
