986ada1c1ef659fd4f6286e8767831ea4e5c1151
Preisler <preisler.andras@gmail.com>
2023-07-20 21:02:24 +0200

    inti

--- src/main/java/petrik/dev/koczkaBased/controller/QuotController.java
+++ src/main/java/petrik/dev/koczkaBased/controller/QuotController.java
@@ -1,14 +1,14 @@
 package petrik.dev.koczkaBased.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class HttpController {
+@RestController
+public class QuotController {
 
-    @GetMapping("/")
+    @GetMapping("/quotation")
     public String helloWorld(@RequestParam(required = false, defaultValue = "index") String param, Model model) {
         model.addAttribute("message", param);
         return "index";
