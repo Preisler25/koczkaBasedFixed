f4da62dd8ae75e99087ad30ad14d0dd6bd85ac5a
Preisler <preisler.andras@gmail.com>
2023-07-23 22:05:11 +0200

    planin'

--- src/main/java/petrik/dev/koczkaBased/util/Quot.java
+++ src/main/java/petrik/dev/koczkaBased/util/Quot.java
@@ -1,6 +1,9 @@
 package petrik.dev.koczkaBased.util;
 
 public class Quot {
+
+    //constructor needed for later implementation of multiple teachers
+
     String[] messages = {"Soha ne feled el levédeni a telnet portodat!!", "Tudja az üvegbeton életett is menthet!", "Ha valaha is gondot okoz a relé müködése csak gondoljon a teknősőkre!", "Aki almát eszik az egészséges marad! Ezt jegyezze meg!", "Egy kis áramkör is lehet hibás, ha hülye aki csinálja,", "Okoskák inkább csöndbe maradjanak!", "Ne feledjék amit holnap el tudnak halasztani azon ma nem éri meg gondolkodni!"};
     public String getQuot() throws Exception {
         return generateMessage();
